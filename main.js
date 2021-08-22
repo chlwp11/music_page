e30190eeeb595254909906fde78eab482f3b2432
chlwp11 <wpwns__@naver.com>
2021-08-22 23:20:46 +0900

    upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const dropdown = document.querySelector('.dropdown')
+const toggleButton = document.querySelector('.dropdown-toggle')
+const menu = document.querySelector('.dropdown-menu')
+const options = document.querySelectorAll('.dropdown-option')
+const nextButton = document.querySelector('.next-button')
+// 토글버튼이 클릭 -> 메뉴를 보여준다.
+// 메뉴 max-height: 0 
+// 메뉴.show ->해지
+toggleButton.addEventListener('click',function(){
+    menu.classList.toggle('show')
+})
+
+toggleButton.addEventListener('blur',function(){
+    menu.classList.remove('show')
+})
+// blur 선택된 영역을 해제 시킬때 사용 
+
+options.forEach(function(item){
+   item.addEventListener('click', function(e){
+       const value = e.currentTarget.textContent.trim()
+       toggleButton.textContent = value
+       toggleButton.classList.add('selected')
+       nextButton.removeAttribute('disabled')
+   })
+})
