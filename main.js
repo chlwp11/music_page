402a158cf612cd0449273d1500fbd277f01428b1
chlwp11 <wpwns__@naver.com>
2021-09-08 16:11:37 +0900

    [upload]playlist 작업

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const dropdown = document.querySelector('.dropdown')
-const toggleButton = document.querySelector('.dropdown-toggle ')
-const play = document.querySelector('.play')
-const menu = document.querySelector('.dropdown-menu')
-const options = document.querySelectorAll('.dropdown-option')
-const playButton = document.querySelector('.play-button')
-const stopButton = document.querySelector('.stop-button')
+const dropdown = document.querySelector('.dropdown');
+const toggleButton = document.querySelector('.dropdown-toggle ');
+const play = document.querySelector('.play');
+const menu = document.querySelector('.dropdown-menu');
+const options = document.querySelectorAll('.dropdown-option');
+const playButton = document.querySelector('.play-button');
+const stopButton = document.querySelector('.stop-button');
+const audio1 = document.querySelector('.audio1');
+const audio2 = document.querySelector('.audio2');
+const audio3 = document.querySelector('.audio3');
+const audio4 = document.querySelector('.audio4');
+const audio5 = document.querySelector('.audio5');
 // 토글버튼이 클릭 -> 메뉴를 보여준다.
 // 메뉴 max-height: 0 
 // 메뉴.show ->해지
@@ -22,19 +27,39 @@ options.forEach(function(item){
        toggleButton.textContent = value
        toggleButton.classList.add('selected')
        playButton.removeAttribute('disabled')
+       playButton.style.display ="block";
+       stopButton.style.display ="none";
     })
 })
 /* play1 */
 const one_dropdown = document.querySelector('.one_dropdown');
 const play_1 = document.querySelector('.play_1');
 one_dropdown.onclick = showlist1;
-
 function showlist1(){  
     play_1.style.display = "block";
     play_2.style.display = "none";
     play_3.style.display = "none";
     play_4.style.display = "none";
     play_5.style.display = "none";
+    audio2.pause();
+    audio2.currentTime = 0;
+    audio3.pause();
+    audio3.currentTime = 0;
+    audio4.pause();
+    audio4.currentTime = 0;
+    audio5.pause();
+    audio5.currentTime = 0;
+    audio1.play();
+    audio1.volume = 0.5;
+    playButton.style.display = "none";
+    stopButton.style.display = "block";
+    playButton.addEventListener('click',function(){
+        audio1.play();
+        audio2.pause();
+        audio3.pause();
+        audio4.pause();
+        audio5.pause();
+    })
 };
 /* play2 */
 const two_dropdown = document.querySelector('.two_dropdown');
@@ -47,6 +72,25 @@ function showlist2(){
     play_3.style.display = "none";
     play_4.style.display = "none";
     play_5.style.display = "none";
+    audio1.pause();
+    audio1.currentTime = 0;
+    audio3.pause();
+    audio3.currentTime = 0;
+    audio4.pause();
+    audio4.currentTime = 0;
+    audio5.pause();
+    audio5.currentTime = 0;
+    audio2.play();
+    audio2.volume = 0.5;
+    playButton.style.display = "none";
+    stopButton.style.display = "block";
+    playButton.addEventListener('click',function(){
+        audio2.play();
+        audio1.pause();
+        audio3.pause();
+        audio4.pause();
+        audio5.pause();
+    })
 };
 /* play3 */
 const three_dropdown = document.querySelector('.three_dropdown');
@@ -59,6 +103,25 @@ function showlist3(){
     play_2.style.display = "none";
     play_4.style.display = "none";
     play_5.style.display = "none";
+    audio1.pause();
+    audio1.currentTime = 0;
+    audio2.pause();
+    audio2.currentTime = 0;
+    audio4.pause();
+    audio4.currentTime = 0;
+    audio5.pause();
+    audio5.currentTime = 0;
+    audio3.play();
+    audio3.volume = 0.5;
+    playButton.style.display = "none";
+    stopButton.style.display = "block";
+    playButton.addEventListener('click',function(){
+        audio3.play();
+        audio2.pause();
+        audio1.pause();
+        audio4.pause();
+        audio5.pause();
+    })
 };
 /* play4 */
 const four_dropdown = document.querySelector('.four_dropdown');
@@ -71,10 +134,30 @@ function showlist4(){
     play_2.style.display = "none";
     play_3.style.display = "none";
     play_5.style.display = "none";
+    audio1.pause();
+    audio1.currentTime = 0;
+    audio2.pause();
+    audio2.currentTime = 0;
+    audio3.pause();
+    audio3.currentTime = 0;
+    audio5.pause();
+    audio5.currentTime = 0;
+    audio4.play();
+    audio4.volume = 0.5;
+    playButton.style.display = "none";
+    stopButton.style.display = "block";
+    playButton.addEventListener('click',function(){
+        audio4.play();
+        audio2.pause();
+        audio3.pause();
+        audio1.pause();
+        audio5.pause();
+    })
 };
 /* play5 */
 const five_dropdown = document.querySelector('.five_dropdown');
 const play_5 = document.querySelector('.play_5');
+
 five_dropdown.onclick = showlist5;
 function showlist5(){ 
     play_5.style.display = "block";
@@ -82,29 +165,38 @@ function showlist5(){
     play_2.style.display = "none";
     play_3.style.display = "none";
     play_4.style.display = "none";
+    audio1.pause();
+    audio1.currentTime = 0;
+    audio2.pause();
+    audio2.currentTime = 0;
+    audio3.pause();
+    audio3.currentTime = 0;
+    audio4.pause();
+    audio4.currentTime = 0;
+    audio5.play();
+    audio5.volume = 0.5;
+    playButton.style.display = "none";
+    stopButton.style.display = "block";
+    playButton.addEventListener('click',function(){
+        audio5.play();
+        audio2.pause();
+        audio3.pause();
+        audio4.pause();
+        audio1.pause();
+    })
 };
+playButton.addEventListener('click',function(){
+    playButton.style.display = "none";
+    stopButton.style.display = "block";
 
-function play1() {
-    const audio1 = document.querySelector(".audio1");
-    audio1.play()
-}
-function play2() {
-    const audio2 = document.querySelector(".audio2");
-    audio2.play()
-}
-function play3() {
-    const audio3 = document.querySelector(".audio3");
-    audio3.play()
-}
-function play4() {
-    const audio4 = document.querySelector(".audio4");
-    audio4.play()
-}
-function play5() {
-    const audio5 = document.querySelector(".audio5");
-    audio5.play()
-}
-playButton.querySelector('click',function(){
-    stopButton.classList.add('block')
-    playButton.classList.add('none')
+
+})
+stopButton.addEventListener('click',function(){
+    playButton.style.display = "block";
+    stopButton.style.display = "none";
+    audio1.pause();
+    audio2.pause();
+    audio3.pause();
+    audio4.pause();
+    audio5.pause();
 })
